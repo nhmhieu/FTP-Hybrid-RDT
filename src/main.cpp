57a540b739cbc2156c77431335f283eac632e7be
phmtuan2522-coder <phanhuynhminhtuan44@gmail.com>
2026-07-28 13:19:49 +0700

    chinh sua lai dinh dang folder va sua lai ham main

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,32 @@
-#include <iostream>
+include <iostream>
 #include <string>
-#include "control/TCPServer.h"// Nhúng header Server của bạn vào đây
+#include "control/TCPServer.h"
+
 #pragma comment(lib, "Ws2_32.lib")
 
-
 int main(int argc, char* argv[]) {
-    bool isServer = false;
-    bool isClient = false;
-    int port = 8080;           // Cổng mặc định
-    std::string host = "127.0.0.1"; // IP mặc định
+    int port = 8080; // Cổng mặc định
 
-    // 1. Vòng lặp duyệt và phân tích các tham số dòng lệnh (argv)
+    // 1. Phân tích tham số dòng lệnh (Command Line Arguments)
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
 
-        if (arg == "-s") {
-            isServer = true;
-        }
-        else if (arg == "-c") {
-            isClient = true;
-        }
-        else if (arg == "-p" && i + 1 < argc) {
+        if (arg == "-p" && i + 1 < argc) {
             port = std::stoi(argv[++i]); // Đọc giá trị port phía sau -p
         }
-        else if (arg == "-h" && i + 1 < argc) {
-            host = argv[++i];            // Đọc giá trị host phía sau -h
-        }
     }
 
-    // 2. Ràng buộc logic kiểm tra đầu vào
-    if (isServer && isClient) {
-        std::cerr << "Lỗi: Không thể chạy vừa là Server vừa là Client cùng lúc!\n";
-        return 1;
-    }
+    // 2. Khởi chạy Server
+    std::cout << "===> Dang khoi chay TCP Server o Port: " << port << "...\n";
 
-    // 3. Điều hướng thực thi
-    if (isServer) {
-        std::cout << "--> Dang khoi chạy TCP Server o Port: " << port << "...\n";
-        // TCPServer server(port);
-        // server.start();
-        // server.acceptClients();
-    }
-    else if (isClient) {
-        std::cout << "--> Dang khoi chạy TCP Client ket noi toi " << host << ":" << port << "...\n";
-        // TCPClient client(host, port);
-        // client.connectToServer();
+    TCPServer server(port);
+    if (server.start()) {
+        server.acceptClients();
     }
     else {
-        std::cout << "Cú pháp sử dụng:\n";
-        std::cout << "  Chạy Server: " << argv[0] << " -s -p <port>\n";
-        std::cout << "  Chạy Client: " << argv[0] << " -c -h <host> -p <port>\n";
+        std::cerr << "Khong the khoi tao Server!\n";
+        return 1;
     }
 
     return 0;
 }
-
